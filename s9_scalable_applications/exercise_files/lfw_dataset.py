59c70eadceadf0c8cb995cc87f9f21ef5adb8ca9
dvorado6 <dominikdvoracek1@gmail.com>
2024-01-16 11:38:30 +0100

    data loading solution

--- s9_scalable_applications/exercise_files/lfw_dataset.py
+++ s9_scalable_applications/exercise_files/lfw_dataset.py
@@ -1,11 +1,16 @@
 """LFW dataloading."""
 import argparse
+import os
 import time
 
 import numpy as np
 import torch
+from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
+import torchvision
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 class LFWDataset(Dataset):
@@ -13,23 +18,31 @@ class LFWDataset(Dataset):
 
     def __init__(self, path_to_folder: str, transform) -> None:
         # TODO: fill out with what you need
+        self.path_to_folder = path_to_folder
         self.transform = transform
+        self.files = []
+        for root, dirs, files in os.walk(path_to_folder):
+            for file in files:
+                self.files.append(os.path.join(root, file))
+        self.len = len(self.files)
 
     def __len__(self):
         """Return length of dataset."""
-        return None  # TODO: fill out
+        return self.len
 
     def __getitem__(self, index: int) -> torch.Tensor:
         """Get item from dataset."""
-        # TODO: fill out
-        return self.transform(img)
+        img_path = self.files[index]
+        img = Image.open(img_path)
+        img = self.transform(img)
+        return img
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-path_to_folder", default="", type=str)
+    parser.add_argument("-path_to_folder", default="lfw-deepfunneled", type=str)
     parser.add_argument("-batch_size", default=512, type=int)
-    parser.add_argument("-num_workers", default=None, type=int)
+    parser.add_argument("-num_workers", default=1, type=int)
     parser.add_argument("-visualize_batch", action="store_true")
     parser.add_argument("-get_timing", action="store_true")
     parser.add_argument("-batches_to_check", default=100, type=int)
@@ -45,13 +58,20 @@ if __name__ == "__main__":
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
     if args.visualize_batch:
-        # TODO: visualize a batch of images
-        pass
+        if args.visualize_batch:
+            # Get one batch from the dataloader
+            batch = next(iter(dataloader))
+
+            # Visualize the batch of images
+            grid = torchvision.utils.make_grid(batch)
+            plt.imshow(grid.permute(1, 2, 0))
+            plt.axis('off')
+            plt.show()
 
     if args.get_timing:
         # lets do some repetitions
         res = []
-        for _ in range(5):
+        for _ in tqdm(range(5)):
             start = time.time()
             for batch_idx, _batch in enumerate(dataloader):
                 if batch_idx > args.batches_to_check:
@@ -61,4 +81,4 @@ if __name__ == "__main__":
             res.append(end - start)
 
         res = np.array(res)
-        print("Timing: {np.mean(res)}+-{np.std(res)}")
+        print(f"Timing: {np.mean(res)}+-{np.std(res)}")
